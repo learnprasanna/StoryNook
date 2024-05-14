8e927d460ee06c3d1469e848a2322e26820f4561
krishnapranayangara <krishnapranay.angara@gmail.com>
2024-05-13 22:18:45 -0400

    sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ const StorynookLogin = () => {
           type="submit"
           className="text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         >
-          Login
+          Logins
         </button>
       </form>
     </div>
